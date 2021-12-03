642da0079d061dbbc4b4e6926408267f1d70a4fe
Kyle Mayer <x@kylemayer.dev>
2021-12-02 16:32:20 -0800

    homepage image

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,34 @@
 import styles from "../styles/Home.module.css";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
         <h1 className={styles.title}>Auth 🪄</h1>
+        <Image
+          src={"/magic_black.svg"}
+          alt="Magic Black"
+          width={150}
+          height={100}
+        />
         <span>
-          powered by <a href="https://magic.link/">Magic Link</a>,{" "}
-          <a href="https://nextjs.org/">Next.Js</a>,{" "}
-          <a href="https://supabase.com/">Supabase</a>,{" "}
-          <a href="https://tailwindcss.com/">Tailwind CSS</a>
+          powered by{" "}
+          <a href="https://magic.link/" rel="noreferrer" target="_blank">
+            Magic Link
+          </a>
+          ,{" "}
+          <a href="https://nextjs.org/" rel="noreferrer" target="_blank">
+            Next.Js
+          </a>
+          ,{" "}
+          <a href="https://supabase.com/" rel="noreferrer" target="_blank">
+            Supabase
+          </a>
+          ,{" "}
+          <a href="https://tailwindcss.com/" rel="noreferrer" target="_blank">
+            Tailwind CSS
+          </a>
         </span>
       </main>
     </div>
